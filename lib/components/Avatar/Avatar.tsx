0e9909f6876e416d8fe76825c2d982fbf3590bd4
sridhar janardhan <sridhar.janardhan@westwing.de>
2024-01-31 13:14:14 +0530

    fix: avatar props

--- lib/components/Avatar/Avatar.tsx
+++ lib/components/Avatar/Avatar.tsx
@@ -1,22 +1,23 @@
 import { styled } from 'styled-components';
 import StyledThemeProvider from '../../Theme/StyledThemeProvider';
 
-const StyledImage = styled("img")<{enableShadow: boolean}>`
+const StyledImage = styled("img") <{ enableShadow: boolean }>`
     width: 3.5rem;
     height: 3.5rem;
     border-radius: 50px;
     border: ${({ theme }) => `2px solid ${theme.colors.$borderColor}; `};
-    box-shadow: ${({enableShadow}) => enableShadow ? `2px 2px` : ''};
+    box-shadow: ${({ enableShadow }) => enableShadow ? `2px 2px` : ''};
 `
 
 interface AvatarProps {
   enableShadow?: boolean
+  imageUrl?: string
 }
 
-export const Avatar = ({enableShadow = true}:AvatarProps) => {
+export const Avatar = ({ enableShadow = true, imageUrl }: AvatarProps) => {
   return (
     <StyledThemeProvider>
-        <StyledImage enableShadow={enableShadow} src='https://image-service-sridhar.s3.us-east-1.amazonaws.com/1703618921140.jpeg' />
+      <StyledImage enableShadow={enableShadow} src={imageUrl} />
     </StyledThemeProvider>
   )
 }
