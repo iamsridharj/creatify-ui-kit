b26beba9ac4d074666336c6b718f275b1c242d5d
sridhar janardhan <sridhar.janardhan@westwing.de>
2024-01-31 16:14:24 +0530

    Fix: fixed img tag

--- lib/components/Image/Image.tsx
+++ lib/components/Image/Image.tsx
@@ -2,20 +2,22 @@ import { HtmlHTMLAttributes } from 'react';
 import { styled } from 'styled-components';
 import StyledThemeProvider from '../../Theme/StyledThemeProvider';
 
-const StyledImage = styled("img")<{enableShadow: boolean}>`
+const StyledImage = styled("img") <{ enableShadow: boolean }>`
+  max-width: 100%;
+  height: auto;
   border: ${({ theme }) => `2px solid ${theme.colors.$borderColor}; `};
-  box-shadow: ${({enableShadow}) => enableShadow ? `4px 4px` : ''};
+  box-shadow: ${({ enableShadow }) => enableShadow ? `4px 4px` : ''};
 `;
 
-interface ImageProps extends HtmlHTMLAttributes<HTMLImageElement>{
+interface ImageProps extends HtmlHTMLAttributes<HTMLImageElement> {
   imageUrl: string
   enableShadow?: boolean;
 }
 
-export const Image = ({imageUrl = "https://image-service-sridhar.s3.us-east-1.amazonaws.com/1703618921140.jpeg", enableShadow = true, ...props}: ImageProps) => {
+export const Image = ({ imageUrl = "https://image-service-sridhar.s3.us-east-1.amazonaws.com/1703618921140.jpeg", enableShadow = true, ...props }: ImageProps) => {
   return (
     <StyledThemeProvider>
-      <StyledImage width={300} height={300} enableShadow={enableShadow} src={imageUrl} {...props} />
+      <StyledImage enableShadow={enableShadow} src={imageUrl} {...props} />
     </StyledThemeProvider>
   )
 }
