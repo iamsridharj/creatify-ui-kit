8e3f66bfce1d6cc5d17967b39c5df0ee044771ba
sridhar janardhan <sridhar.janardhan@westwing.de>
2024-01-31 13:25:57 +0530

    fix: avatar props

--- lib/components/Avatar/Avatar.tsx
+++ lib/components/Avatar/Avatar.tsx
@@ -2,8 +2,6 @@ import { styled } from 'styled-components';
 import StyledThemeProvider from '../../Theme/StyledThemeProvider';
 
 const StyledImage = styled("img") <{ enableShadow: boolean }>`
-    width: 3.5rem;
-    height: 3.5rem;
     border-radius: 50px;
     border: ${({ theme }) => `2px solid ${theme.colors.$borderColor}; `};
     box-shadow: ${({ enableShadow }) => enableShadow ? `2px 2px` : ''};
